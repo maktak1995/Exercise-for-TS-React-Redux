f4914cdbd55ba4d5449020735270fc087005ee9b
maktak1995 <takehiro_makita@voyagegroup.com>
2020-04-24 11:38:00 +0900

    redux-sampleを追加

---
+++ redux-sample/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Provider } from "react-redux";
+import './App.css';
+import { CounterView } from "./pages/CounterView";
+import { Employees } from "./pages/Employees";
+import { configureStore } from "./store";
+
+function App() {
+  const store = configureStore();
+  return (
+    <Provider store={store}>
+      <CounterView />
+      <Employees />
+    </Provider>
+  );
+}
+
+export default App;
